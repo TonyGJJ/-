249a46ee487368f08307d08b403cdff99caef87e
刘杨 <>
2021-03-25 15:33:28 +0800

    完成中缀表达式转后缀表达式

--- StackAndQueue/StackAndQueue/Expression.m
+++ StackAndQueue/StackAndQueue/Expression.m
@@ -10,6 +10,7 @@
 
 @implementation Expression {
     NSMutableArray *_infixArray;
+    OneStack *stack;
 }
 
 - (instancetype)init
@@ -17,6 +18,8 @@
     self = [super init];
     if (self) {
         _infixArray = [[NSMutableArray alloc] init];
+        stack = [[OneStack alloc] init];
+        [stack createStack];
     }
     return self;
 }
@@ -25,22 +28,22 @@
 - (void)test {
     // input 9+(3-1)×3+10÷2
     // ouput 9 3 1 - 3 * + 10 2 / +
-    OneStack *stack = [[OneStack alloc] init];
-    [stack createStack];
-    NSMutableArray *ss = @[@"9",
-                           @"+",
-                           @"(",
-                           @"3",
-                           @"-",
-                           @"1",
-                           @")",
-                           @"x",
-                           @"3",
-                           @"+",
-                           @"10",
-                           @"÷",
-                           @"2"
-                         ].mutableCopy;
+    
+    NSArray *ss = @[
+        @"9",
+        @"+",
+        @"(",
+        @"3",
+        @"-",
+        @"1",
+        @")",
+        @"x",
+        @"3",
+        @"+",
+        @"10",
+        @"÷",
+        @"2"
+    ];
     
     for (int i = 0; i < [ss count]; i++) {
         NSString *temp = ss[i];
@@ -73,21 +76,90 @@
                 // 处理运算先后
                 NSString *temp2 = [stack getTopStackData];
                 NSString *temp3 = [self comparePriorityPriority:temp twoPriority:temp2];
-                [stack push:temp];
+                NSString *symbol = [self comparePriorityWithTop:temp];
+                
+                if ([symbol isEqualToString:@"<"]) {
+                    NSString *newTemp = [stack getTopStackData];
+                    while (newTemp.length != 0) {
+                        [_infixArray addObject:newTemp];
+                        [stack pop];
+                        newTemp = [stack getTopStackData];
+                    }
+                    [stack push:temp];
+                } else {
+                    if ([symbol isEqualToString:@"=="]) {
+                        [stack push:temp];
+                    } else {
+                        if (temp2 == temp3) {
+                            [_infixArray addObject:temp3];
+                        } else {
+                            [stack push:temp];
+                        }
+                    }
+                }
             }
         }
+    }
+    
+    NSString *temp2 = [stack getTopStackData];
+    while (temp2.length != 0) {
+        [_infixArray addObject:temp2];
+        [stack pop];
+        temp2 = [stack getTopStackData];
+    }
+    
+    NSLog(@"%@", [_infixArray componentsJoinedByString:@" "]);
+}
+
+
+/// 和栈顶比较优先级
+/// YES 表示 优先级高于栈顶
+/// NO  表示 优先级低于栈顶
+/// @param symbol 符号
+- (NSString *)comparePriorityWithTop:(NSString *)symbol {
+    NSString *topSymbol = [stack getTopStackData];
+    
+    if ([topSymbol isEqualToString:@"("] || [topSymbol isEqualToString:@")"]) {
+        return @"==";
+    }
+    
+    if ([symbol isEqualToString:@"("] || [symbol isEqualToString:@")"]) {
+        return @"==";
+    }
+    
+    int symbolInt = 0;
+    int topStackInt = 0;
+    
+    if ([symbol isEqualToString:@"*"] ||
+        [symbol isEqualToString:@"/"]) {
+        symbolInt = 2;
+    } else if ([symbol isEqualToString:@"+"] ||
+               [symbol isEqualToString:@"-"]) {
+        symbolInt = 1;
+    }
+    
+    if ([topSymbol isEqualToString:@"+"] ||
+        [topSymbol isEqualToString:@"-"]) {
+        topStackInt = 1;
+    } else if ([topSymbol isEqualToString:@"*"] ||
+               [topSymbol isEqualToString:@"/"]) {
+        topStackInt = 2;
+    }
+    
+    if (symbolInt == topStackInt) {
+        return @"==";
+    } else if (symbolInt > topStackInt) {
+        return @">";
+    } else {
+        return @"<";
     }
 }
 
 - (NSString *)comparePriorityPriority:(NSString *)one twoPriority:(NSString *)two {
-    if ([one isEqualToString:@"*"] && [two isEqualToString:@"+"]) {
-        return one;
-    } else if([one isEqualToString:@"*"] && [two isEqualToString:@"-"]) {
-        return one;
-    } else if([one isEqualToString:@"/"] && [two isEqualToString:@"+"]) {
-        return one;
-    } else if([one isEqualToString:@"/"] && [two isEqualToString:@"-"]) {
-        return one;
+    if ([one isEqualToString:@"*"] || [two isEqualToString:@"*"]) {
+        return @"*";
+    } else if([one isEqualToString:@"/"] || [two isEqualToString:@"/"]) {
+        return @"/";
     } else if ([one isEqualToString: two]){
         return one;
     }
